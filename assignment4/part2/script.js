38289b519d94b21cb4c020e8f7be6c5156a4ef3f
edgarestuardosandoval-a11y <edgarestuardosandoval@gmail.com>
2025-11-28 02:14:26 -0500

    Created a data object

--- assignment4/part2/script.js
+++ assignment4/part2/script.js
@@ -3,3 +3,10 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+const images = [
+  { filename: "pic1.jpg", alt: "A Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "A Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Some purple and white pansies" },
+  { filename: "pic4.jpg", alt: "A section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "A large moth on a leaf" },
+];
