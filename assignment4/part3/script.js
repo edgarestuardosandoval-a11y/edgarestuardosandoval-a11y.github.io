19f419b429e9750b35dec5c67850a72cb7ab39bc
edgarestuardosandoval-a11y <edgarestuardosandoval@gmail.com>
2025-11-28 02:31:03 -0500

    Update script.js
    added draw and update to ball class

--- assignment4/part3/script.js
+++ assignment4/part3/script.js
@@ -26,4 +26,31 @@ class Ball {
     this.color = color;
     this.size = size;
   }
-}
+
+  draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  update() {
+      if (this.x + this.size >= width) {
+        this.velX = -Math.abs(this.velX);
+      }
+
+      if (this.x - this.size <= 0) {
+        this.velX = Math.abs(this.velX);
+      }
+
+      if (this.y + this.size >= height) {
+        this.velY = -Math.abs(this.velY);
+      }
+
+      if (this.y - this.size <= 0) {
+        this.velY = Math.abs(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+    }
+  }
