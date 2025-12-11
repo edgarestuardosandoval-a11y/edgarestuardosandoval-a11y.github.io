759447b8a6a584069a6141921bb63766ff2c1806
edgarestuardosandoval-a11y <edgarestuardosandoval@gmail.com>
2025-12-10 20:01:10 -0500

    Update main.js
    added function to js ti hide show comments  and added table headers, description and summary

--- assignment5/main.js
+++ assignment5/main.js
@@ -5,16 +5,12 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
-  } else {
-    showHideBtn.textContent = 'Show comments';
-    commentWrapper.style.display = 'none';
+showHideBtn.addEventListener("click", toggleComments);
+showHideBtn.addEventListener("keypress", function(e){
+  if (e.key === "Enter" || e.key === " ") {
+    toggleComments();
   }
-};
+});
 
 // functionality for adding a new comment via the comments form
 
@@ -28,6 +24,17 @@ form.onsubmit = function(e) {
   submitComment();
 };
 
+function toggleComments() {
+  let showHideText = showHideBtn.textContent;
+
+  if (showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+}
 function submitComment() {
   const listItem = document.createElement('li');
   const namePara = document.createElement('p');
